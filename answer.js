32c5376693b4c178aee564e05dd2b92866ad6532
Joao Vitor <joao_vitor_araki_goncalves@hotmail.com>
2017-10-01 01:26:41 -0300

    saving wrong answers too

--- answer.js
+++ answer.js
@@ -25,90 +25,89 @@ firebase.initializeApp(config);
 const splitName = document.getElementsByClassName("page-header-headings")[0].childNodes[0].innerHTML.split(" ")
 const name = splitName[0] + splitName[1]
 
-var db = firebase.database().ref(`${name}/`)
 
 Array.prototype.slice.call(document.getElementsByClassName("formulation")).forEach(e => {
     let question = Array.prototype.slice.call(e.childNodes).filter(e => e.nodeName === "DIV").filter(div => {
-        return div.className === "qtext"
-    })[0]
+    return div.className === "qtext"
+})[0]
 
-    let questionHash = hashCode(new XMLSerializer().serializeToString(question))
+let questionHash = hashCode(new XMLSerializer().serializeToString(question))
 
-    db.child(questionHash).on('value', function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-            Array.prototype.slice.call(question.parentNode.childNodes).filter(e => {
-                    return e.className === "ablock"
-                })
-                .forEach(e => Array.prototype.slice.call(e.childNodes)
-                    .filter(e => {
-                        return e.className === "answer"
-                    })
-                    .forEach(e => {
-                        Array.prototype.slice.call(e.childNodes).map(e => Array.prototype.slice.call(e.childNodes).filter(e => {
-                            return e.nodeName === "LABEL" || e.nodeName === "SPAN"
-                        }).filter(e => {
-                            let answer = e.innerHTML
-                            if (!typeof answer === "string") {
-                                answer = new XMLSerializer().serializeToString(answer)
-                            }
+let db = firebase.database().ref(`${name}/`)
 
-                            if (answer === childSnapshot.val()) {
-                                const input = Array.prototype.slice.call(e.parentNode.childNodes).filter(e => {
-                                  return e.nodeName === "INPUT"
-                                })[0]
-
-                                if (input.type === "radio") {
-                                  input.checked = true
-                                }
-
-                            }
-                        }))
-                    })
-                )
-
-
+db.child(questionHash).on('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+        Array.prototype.slice.call(question.parentNode.childNodes).filter(e => {
+            return e.className === "ablock"
         })
-    })
-
-})
-
-
-Array.prototype.slice.call(document.getElementsByClassName("questioncorrectnessicon"))
-    .map(e => {
-        const answer =
-            Array.prototype.slice.call(e.parentNode.childNodes).filter(e => {
-                return e.nodeName === "LABEL" || e.nodeName === "SPAN" || e.nodeName === "INPUT"
-            }).map(e => {
-                if (e.nodeName === "INPUT") {
-                    return e.value
-                } else {
-                    return e.innerHTML
-                }
-            })[0]
-
-        if (!typeof answer === "string") {
+    .forEach(e => Array.prototype.slice.call(e.childNodes)
+            .filter(e => {
+            return e.className === "answer"
+        })
+    .forEach(e => {
+            Array.prototype.slice.call(e.childNodes).map(e => Array.prototype.slice.call(e.childNodes).filter(e => {
+            return e.nodeName === "LABEL" || e.nodeName === "SPAN"
+        }).filter(e => {
+            let answer = e.innerHTML
+            if (!typeof answer === "string") {
             answer = new XMLSerializer().serializeToString(answer)
         }
 
+        if (answer === childSnapshot.val()) {
+            const input = Array.prototype.slice.call(e.parentNode.childNodes).filter(e => {
+                return e.nodeName === "INPUT"
+            })[0]
 
-
-        let parent = e.parentNode;
-
-        while (parent.className.indexOf("formulation") === -1) {
-            parent = parent.parentNode
+            if (input.type === "radio") {
+                input.checked = true
+                e.style.background = "green"
+            }
 
         }
-
-        const question = Array.prototype.slice.call(parent.childNodes).filter(e => {
-            return e.nodeName === "DIV"
-        }).filter(div => {
-            return div.className === "qtext"
-        })[0]
-
-        console.log(question)
-
-        db.child(hashCode(new XMLSerializer().serializeToString(question))).set({
-            answer: answer,
-            question: new XMLSerializer().serializeToString(question)
-        });
+    }))
     })
+    )
+
+
+    })
+})
+
+db = firebase.database().ref(`${name}/${questionHash}/incorrect`)
+
+db.on('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+        Array.prototype.slice.call(question.parentNode.childNodes).filter(e => {
+            return e.className === "ablock"
+        })
+    .forEach(e => Array.prototype.slice.call(e.childNodes)
+            .filter(e => {
+            return e.className === "answer"
+        })
+    .forEach(e => {
+            Array.prototype.slice.call(e.childNodes).map(e => Array.prototype.slice.call(e.childNodes).filter(e => {
+            return e.nodeName === "LABEL" || e.nodeName === "SPAN"
+        }).filter(e => {
+            let answer = e.innerHTML
+            if (!typeof answer === "string") {
+            answer = new XMLSerializer().serializeToString(answer)
+        }
+
+        if (answer === childSnapshot.val().answer) {
+            const input = Array.prototype.slice.call(e.parentNode.childNodes).filter(e => {
+                return e.nodeName === "INPUT"
+            })[0]
+
+            if (input.type === "radio") {
+                e.style.background = "red"
+            }
+
+        }
+    }))
+    })
+    )
+
+
+    })
+})
+
+})
--- scan.js
+++ scan.js
@@ -1,13 +1,13 @@
-
 const hashCode = function(word) {
-  var hash = 0, i, chr;
-  if (word.length === 0) return hash;
-  for (i = 0; i < word.length; i++) {
-    chr   = word.charCodeAt(i);
-    hash  = ((hash << 5) - hash) + chr;
-    hash |= 0; // Convert to 32bit integer
-  }
-  return hash;
+    var hash = 0,
+        i, chr;
+    if (word.length === 0) return hash;
+    for (i = 0; i < word.length; i++) {
+        chr = word.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
 };
 
 var config = {
@@ -17,7 +17,7 @@ var config = {
     projectId: "fy-moodle",
     storageBucket: "fy-moodle.appspot.com",
     messagingSenderId: "944849265962"
-  };
+};
 firebase.initializeApp(config);
 
 
@@ -25,42 +25,84 @@ firebase.initializeApp(config);
 const splitName = document.getElementsByClassName("page-header-headings")[0].childNodes[0].innerHTML.split(" ")
 const name = splitName[0] + splitName[1]
 
-var db = firebase.database().ref(`${name}/`)
 
 Array.prototype.slice.call(document.getElementsByClassName("questioncorrectnessicon"))
-.filter(e => {
-  return e.src.indexOf("incorrect") === -1
+    .filter(e => {
+    return e.src.indexOf("incorrect") === -1
 })
 .map(e => {
-  const answer =
-  Array.prototype.slice.call(e.parentNode.childNodes).filter(e => {
-    return e.nodeName === "LABEL" || e.nodeName === "SPAN"
-  }).map(e => {
+    const answer =
+        Array.prototype.slice.call(e.parentNode.childNodes).filter(e => {
+            return e.nodeName === "LABEL" || e.nodeName === "SPAN"
+        }).map(e => {
     if (e.nodeName === "INPUT") {
-      return e.value
-    } else {
-      return e.innerHTML
-    }
-  })[0]
+    return e.value
+} else {
+    return e.innerHTML
+}
+})[0]
 
-  if (!typeof answer === "string") {
+if (!typeof answer === "string") {
     answer = new XMLSerializer().serializeToString(answer)
-  }
+}
 
-  let parent = e.parentNode;
+let parent = e.parentNode;
 
-  while (parent.className.indexOf("formulation") === -1) {
+while (parent.className.indexOf("formulation") === -1) {
     parent = parent.parentNode
 
-  }
+}
 
-  const question = Array.prototype.slice.call(parent.childNodes).filter(e => {
+const question = Array.prototype.slice.call(parent.childNodes).filter(e => {
     return e.nodeName === "DIV"
-  }).filter(div => {
+}).filter(div => {
     return div.className === "qtext"
-  })[0]
+})[0]
 
-  db.child(hashCode(new XMLSerializer().serializeToString(question))).set(
-    {answer: answer}
-  );
+const db = firebase.database().ref(`${name}/`)
+
+db.child(hashCode(new XMLSerializer().serializeToString(question))).set({
+    answer: answer
+});
+})
+
+Array.prototype.slice.call(document.getElementsByClassName("questioncorrectnessicon"))
+    .filter(e => {
+    return e.src.indexOf("incorrect") !== -1
+})
+.map(e => {
+    const answer =
+        Array.prototype.slice.call(e.parentNode.childNodes).filter(e => {
+            return e.nodeName === "LABEL" || e.nodeName === "SPAN"
+        }).map(e => {
+    if (e.nodeName === "INPUT") {
+    return e.value
+} else {
+    return e.innerHTML
+}
+})[0]
+
+if (!typeof answer === "string") {
+    answer = new XMLSerializer().serializeToString(answer)
+}
+
+let parent = e.parentNode;
+
+while (parent.className.indexOf("formulation") === -1) {
+    parent = parent.parentNode
+
+}
+
+const question = Array.prototype.slice.call(parent.childNodes).filter(e => {
+    return e.nodeName === "DIV"
+}).filter(div => {
+    return div.className === "qtext"
+})[0]
+
+const questionHash = hashCode(new XMLSerializer().serializeToString(question))
+
+const db = firebase.database().ref(`${name}/${questionHash}/incorrect`)
+db.child(hashCode(answer)).set({
+    answer: answer
+});
 })
