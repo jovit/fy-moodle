6cbbcce9798577bf4c6b961761a296d252436acc
Joao Vitor Araki Goncalves <joao.goncalves123456789@gmail.com>
2017-10-02 00:34:37 -0300

    adding feedback

--- moonshot.js
+++ moonshot.js
@@ -21,9 +21,17 @@ var config = {
 }
 firebase.initializeApp(config)
 
+document.body.innerHTML +=
+  '<div id="loading-popup" style="text-align:center;display:fixed;position:absolute;bottom:0px;width:100%;height:50px;background:yellow;z-index:90; padding-top:15px;color=#000">Carregando os seus moodles antigos...</div>'
+
 const classes = Array.prototype.slice.call(
   document.getElementsByClassName('type_course')
 )
+
+setTimeout(() => {
+  document.getElementById('loading-popup').style.display = 'none'
+}, 10000)
+
 classes.forEach(e => {
   let a = e.childNodes[0].childNodes[0]
   const link = a.href
